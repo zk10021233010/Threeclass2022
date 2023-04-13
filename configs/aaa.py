c0ffc85db84a9d06a07c5f1997adffa8447de23b
zhangkai <1002133010@qq.com>
2023-04-13 21:58:38 +0800

    新增aaa.py

---
+++ configs/aaa.py
@@ -0,0 +1,12 @@
+# author:zhangkai
+# time:2023/4/13
+# -*-coding:utf-8-*-
+
+
+def zhangkai(a,b):
+    if a == b:
+        print("hahhaha")
+    else:
+        print("......")
+    return
+c=zhangkai(1,2)
